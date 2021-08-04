b182854701bf8f9e2b3387b48ba0c21b5d6f4841
Impartiall <42242696+Impartiall@users.noreply.github.com>
2021-08-04 11:51:40 -0400

    Implement controller for /graphql route

---
+++ src/Application/Controllers/GraphQLController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Application\Controllers;
+
+use Doctrine\DBAL\Connection;
+use GraphQL\Executor\Executor;
+use GraphQL\GraphQL;
+use GraphQL\Type\Definition\ResolveInfo;
+use GraphQL\Utils\BuildSchema;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Log\LoggerInterface;
+use Exception;
+
+/**
+ * A controller to handle GraphQL API requests
+ */
+class GraphQLController
+{
+    protected $db;
+    protected $logger;
+
+    public function __construct(Connection $connection, LoggerInterface $logger)
+    {
+        $this->db = $connection;
+        $this->logger = $logger;
+    }
+
+    /**
+     * The only controller action, since all GraphQL requests
+     * are parsed and handled in the same way
+     * 
+     * @param Request $request The incoming request
+     * @param Response $response The provided response object
+     */
+    public function index(Request $request, Response $response) {
+        // Use custom GraphQL resolvers
+        $this->setResolvers(include dirname(__DIR__, 3) . '/src/GraphQL/resolvers.php');
+        
+        // Load schema from file
+        $schema = BuildSchema::build(file_get_contents(dirname(__DIR__, 3) . '/src/GraphQL/schema.graphqls'));
+
+        // Read data passed in request
+        $input = $request->getParsedBody();
+        $query = $input['query'];
+
+        $variables = isset($input['variables']) ? $input['variables'] : null;
+
+        // Pass database connection and logger to resolvers
+        $context = [
+            'db'      => $this->db,
+            'logger'  => $this->logger
+        ];
+
+        // Set the root value to a value of type array
+        $rootValue = [];
+
+        $result = GraphQL::executeQuery($schema, $query, $rootValue, $context, $variables);
+
+        // Generate response from query result
+        $response->getBody()->write(json_encode($result));
+
+        // Log SQL queries
+        $sqlQueryLogger = $this->db->getConfiguration()->getSQLLogger();
+        $this->logger->info(json_encode($sqlQueryLogger->queries));
+
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    /**
+     * Set GraphQL resolver functions from an array
+     * 
+     * @param array $resolvers A map of GraphQL resolver functions
+     */
+    private function setResolvers(array $resolvers) {
+        Executor::setDefaultFieldResolver(function ($source, $args, $context, ResolveInfo $info) use ($resolvers) {
+            $fieldName = $info->fieldName;
+    
+            if (is_null($fieldName)) {
+                throw new Exception('Could not get $fieldName from ResolveInfo');
+            }
+    
+            if (is_null($info->parentType)) {
+                throw new Exception('Could not get $parentType from ResolveInfo');
+            }
+    
+            $parentTypeName = $info->parentType->name;
+    
+            if (isset($resolvers[$parentTypeName])) {
+                $resolver = $resolvers[$parentTypeName];
+    
+                if (is_array($resolver)) {
+                    if (array_key_exists($fieldName, $resolver)) {
+                        $value = $resolver[$fieldName];
+    
+                        return is_callable($value) ? $value($source, $args, $context, $info) : $value;
+                    }
+                }
+    
+                if (is_object($resolver)) {
+                    if (isset($resolver->{$fieldName})) {
+                        $value = $resolver->{$fieldName};
+    
+                        return is_callable($value) ? $value($source, $args, $context, $info) : $value;
+                    }
+                }
+            }
+    
+            return Executor::defaultFieldResolver($source, $args, $context, $info);
+        });
+    }
+}
