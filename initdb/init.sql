98c5e7a557d7a5299f2898234b548ff80507007d
Impartiall <42242696+Impartiall@users.noreply.github.com>
2021-08-04 12:27:39 -0400

    Split 'chats' into 'chats' and 'chanels', and use a different schema generator

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,107 +1,75 @@
 \connect postgres;
 
-CREATE TABLE users (
-	id serial NOT NULL,
-	username varchar(25) NOT NULL,
-	email varchar(255) NOT NULL UNIQUE,
-	password varchar(255) NOT NULL,
-	is_online BOOLEAN NOT NULL DEFAULT false,
-	CONSTRAINT users_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "username" varchar(25) NOT NULL,
+  "email" varchar(255) UNIQUE NOT NULL,
+  "password" varchar(255) NOT NULL,
+  "is_online" BOOLEAN NOT NULL DEFAULT false
 );
 
-
-
-CREATE TABLE messages (
-	id serial NOT NULL,
-	user_id integer NOT NULL,
-	content varchar(500) NOT NULL,
-	chat_id integer NOT NULL,
-	CONSTRAINT messages_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "messages" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "user_id" integer NOT NULL,
+  "content" varchar(500) NOT NULL,
+  "chat_id" integer,
+  "channel_id" integer
 );
 
-
-
-CREATE TABLE chats (
-	id serial NOT NULL,
-	server_id integer,
-	name varchar(25) NOT NULL,
-	CONSTRAINT chats_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "chats" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "name" varchar(25)
 );
 
-
-
-CREATE TABLE messages_users (
-	id serial NOT NULL,
-	read BOOLEAN NOT NULL,
-	user_id integer NOT NULL,
-	message_id integer NOT NULL,
-	CONSTRAINT messages_users_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "channels" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "server_id" integer NOT NULL,
+  "name" varchar(25) NOT NULL
 );
 
-
-
-CREATE TABLE chats_users (
-	id serial NOT NULL,
-	chat_id integer NOT NULL,
-	user_id integer NOT NULL,
-	CONSTRAINT chats_users_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "messages_users" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "read" BOOLEAN NOT NULL,
+  "user_id" integer NOT NULL,
+  "message_id" integer NOT NULL
 );
 
-
-
-CREATE TABLE servers (
-	id serial NOT NULL,
-	name varchar(25) NOT NULL,
-	user_id integer NOT NULL,
-	CONSTRAINT servers_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "chats_users" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "chat_id" integer NOT NULL,
+  "user_id" integer NOT NULL
 );
 
-
-
-CREATE TABLE servers_users (
-	id serial NOT NULL,
-	user_id integer NOT NULL,
-	server_id integer NOT NULL,
-	CONSTRAINT servers_users_pk PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "servers" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "name" varchar(25) NOT NULL,
+  "user_id" integer NOT NULL
 );
 
+CREATE TABLE "servers_users" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "user_id" integer NOT NULL,
+  "server_id" integer NOT NULL
+);
 
+ALTER TABLE "messages" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
+ALTER TABLE "messages" ADD FOREIGN KEY ("chat_id") REFERENCES "chats" ("id");
 
-ALTER TABLE messages ADD CONSTRAINT messages_fk0 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE messages ADD CONSTRAINT messages_fk1 FOREIGN KEY (chat_id) REFERENCES chats(id);
+ALTER TABLE "messages_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-ALTER TABLE chats ADD CONSTRAINT chats_fk0 FOREIGN KEY (server_id) REFERENCES servers(id);
+ALTER TABLE "messages_users" ADD FOREIGN KEY ("message_id") REFERENCES "messages" ("id");
 
-ALTER TABLE messages_users ADD CONSTRAINT messages_users_fk0 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE messages_users ADD CONSTRAINT messages_users_fk1 FOREIGN KEY (message_id) REFERENCES messages(id);
+ALTER TABLE "chats_users" ADD FOREIGN KEY ("chat_id") REFERENCES "chats" ("id");
 
-ALTER TABLE chats_users ADD CONSTRAINT chats_users_fk0 FOREIGN KEY (chat_id) REFERENCES chats(id);
-ALTER TABLE chats_users ADD CONSTRAINT chats_users_fk1 FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE "messages" ADD FOREIGN KEY ("channel_id") REFERENCES "channels" ("id");
 
-ALTER TABLE servers ADD CONSTRAINT servers_fk0 FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE "channels" ADD FOREIGN KEY ("server_id") REFERENCES "servers" ("id");
 
-ALTER TABLE servers_users ADD CONSTRAINT servers_users_fk0 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE servers_users ADD CONSTRAINT servers_users_fk1 FOREIGN KEY (server_id) REFERENCES servers(id);
+ALTER TABLE "chats_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
+ALTER TABLE "servers" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
+ALTER TABLE "servers_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-
-
-
-
-
+ALTER TABLE "servers_users" ADD FOREIGN KEY ("server_id") REFERENCES "servers" ("id");
