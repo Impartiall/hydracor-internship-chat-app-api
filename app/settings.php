ffc56b2aaf7740469ba1db83d3c4348aff921376
Impartiall <42242696+Impartiall@users.noreply.github.com>
2021-08-10 11:45:38 -0400

    Move debug mode to an environment variable and default it to false

--- app/settings.php
+++ app/settings.php
@@ -12,7 +12,7 @@ return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
         SettingsInterface::class => function () {
             return new Settings([
-                'displayErrorDetails' => true, // Should be set to false in production
+                'displayErrorDetails' => getenv('DEBUG') ?: false, // Should be set to false in production
                 'logError'            => false,
                 'logErrorDetails'     => false,
                 'logger' => [
