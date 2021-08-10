0df01a56c1e19813d363dcf50fdd2d5ed4af390b
Impartiall <42242696+Impartiall@users.noreply.github.com>
2021-08-10 09:08:07 -0400

    Document User and Query resolvers

--- src/Auth/Authorization.php
+++ src/Auth/Authorization.php
@@ -2,22 +2,30 @@
 
 namespace App\Auth;
 
+use Doctrine\DBAL\Connection;
+
 /**
  * A grouping of methods for authorizing API access
  */
 class Authorization
 {
     /**
-     * @var array|null The user array representing the authenticated user making the request
+     * @var array|null $requester The user array representing the
+     * authenticated user making the request
      */
-    protected $requester;
+    protected array $requester;
+
+    /**
+     * @var Connection $connection A database connection
+     */
+    protected Connection $connection;
 
     /**
      * Create an instance of the Authorization class from an authenticated user
      * 
      * @param array|null The user array representing the authenticated user making the request
      */
-    public function __construct($user)
+    public function __construct(array $user, Connection $connection)
     {
         $this->requester = $user;
     }
@@ -25,14 +33,33 @@ class Authorization
     /**
      * Validate whether a requester can view private properties on a user
      * 
-     * This authorization requires that the requestor is the user being accessed
+     * This authorization requires that the requester is the user being accessed
      * 
-     * @param int $userId The id of the user the requestor is trying to view
+     * @param int $userId The id of the user the requester is trying to view
      * 
-     * @return bool Whether or not the user is authorized for this action
+     * @return bool Whether or not the requester is authorized for this action
      */
     public function canViewUser(int $userId): bool
     {
         return ($this->requester['id'] ?? null) === $userId;
+    }
+
+    /**
+     * Validate whether a requester can view private properties on a chat
+     * 
+     * This authorization requires that the requester is
+     * a member of the chat being accessed
+     * 
+     * @param int $chatId The id of the chat the requester is trying to view
+     * 
+     * @return bool Whether or not the requester is authorized for this action
+     */
+    public function canViewChat(int $chatId): bool
+    {
+        $memberUserIds = $this->connection->fetchFirstColumn(
+            'SELECT user_id FROM chats_users WHERE chat_id = ?',
+            [$chatId]
+        );
+        return in_array($this->requester['id'], $memberUserIds);
     }
 }
