08cf2dd8694d7255a03ebf146c95a2742409e269
Impartiall <42242696+Impartiall@users.noreply.github.com>
2021-08-23 15:19:34 -0400

    Overhaul authorization helpers

--- src/Auth/Authorization.php
+++ src/Auth/Authorization.php
@@ -2,7 +2,9 @@
 
 namespace App\Auth;
 
+use App\Database\Records;
 use App\Exceptions\AuthorizationException;
+use Doctrine\DBAL\Connection;
 
 /**
  * A grouping of methods for authorizing API access
@@ -10,18 +12,26 @@ use App\Exceptions\AuthorizationException;
 class Authorization
 {
     /**
-     * @var array|null The user array representing the authenticated user making the request
+     * @var array|null $requester The user array representing the authenticated user making the request
      */
     protected $requester;
 
     /**
-     * Create an instance of the Authorization class from an authenticated user
-     * 
-     * @param array|null The user array representing the authenticated user making the request
+     * @var Connection $connection A database connection
      */
-    public function __construct($user)
+    protected Connection $connection;
+
+    /**
+     * Create an instance of the Authorization class
+     * from an authenticated user and database connection
+     * 
+     * @param array|null $user The user array representing the authenticated user making the request
+     * @param Connection $connection A database connection
+     */
+    public function __construct($user, Connection $connection)
     {
         $this->requester = $user;
+        $this->connection = $connection;
     }
 
     /**
@@ -53,7 +63,21 @@ class Authorization
             }
         }
     }
-    
+
+    /**
+     * Validate whether a requester can edit properties on a user
+     * 
+     * This authorization requires that the requester is the user being accessed.
+     * 
+     * @param int $userId The id of the user the requester is trying to edit
+     * 
+     * @return bool Whether or not the requester is authorized for this action
+     */
+    public function canEditUser(int $userId): bool
+    {
+        return ($this->getRequester()['id'] ?? null) === $userId;
+    }
+
     /**
      * Validate whether a requester can view private properties on a user
      * 
@@ -61,10 +85,81 @@ class Authorization
      * 
      * @param int $userId The id of the user the requester is trying to view
      * 
-     * @return bool Whether or not the user is authorized for this action
+     * @return bool Whether or not the requester is authorized for this action
      */
-    public function isAuthForUser(int $userId): bool
+    public function canViewUser(int $userId): bool
     {
-        return ($this->getRequester()['id'] ?? null) === $userId;
+        return $this->canEditUser($userId);
+    }
+
+    /**
+     * Validate whether a requester can edit properties on a server
+     * 
+     * This authorization requires that the requester is the owner of the server being accessed.
+     * 
+     * @param int $serverId The id of the server the requester is trying to edit
+     * 
+     * @return bool Whether or not the requester is authorized for this action
+     */
+    public function canEditServer(int $serverId): bool
+    {
+        $server = Records::selectById($this->connection, 'servers', $serverId);
+        return ($this->getRequester()['id'] ?? null) === $server['owner_id'];
+    }
+
+    /**
+     * Validate whether a requester can view private properties on a server
+     * 
+     * This authorization requires that the requester is a member of the server being accessed.
+     * 
+     * @param int $serverId The id of the server the requester is trying to view
+     * 
+     * @return bool Whether or not the requester is authorized for this action
+     */
+    public function canViewServer(int $serverId): bool
+    {
+        return Records::doesRelationshipExist(
+            $this->connection,
+            'servers_users',
+            'server_id',
+            $serverId,
+            'user_id',
+            $this->getRequester()['id'] ?? null
+        );
+    }
+
+    /**
+     * Validate whether a requester can edit properties on a chat
+     * 
+     * This authorization requires that the requester is a member of the chat being accessed.
+     * 
+     * @param int $chatId The id of the chat the requester is trying to edit
+     * 
+     * @return bool Whether or not the requester is authorized for this action
+     */
+    public function canEditChat(int $chatId): bool
+    {
+        return Records::doesRelationshipExist(
+            $this->connection,
+            'chats_users',
+            'chat_id',
+            $chatId,
+            'user_id',
+            $this->getRequester()['id'] ?? null
+        );
+    }
+
+    /**
+     * Validate whether a requester can view private properties on a chat
+     * 
+     * This authorization requires that the requester is a member of the chat being accessed.
+     * 
+     * @param int $chatId The id of the chat the requester is trying to view
+     * 
+     * @return bool Whether or not the requester is authorized for this action
+     */
+    public function canViewChat(int $chatId): bool
+    {
+        return $this->canEditChat($chatId);
     }
 }
